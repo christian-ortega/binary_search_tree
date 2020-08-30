68a6f8c11749915cc1762a1761e0695eb228738c
Christian Ortega <christianmortega7@gmail.com>
2020-08-30 12:38:31 -0400

    Refactor code

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -8,26 +8,12 @@ end
 
 class BinarySearchTree
 
+  public
+
   attr_reader :root
 
   def initialize(array)
     @root = build_tree(array)
-  end
-
-  def build_tree(array)
-    return nil if array.length == 0
-    return Node.new(array[0]) if array.length == 1
-
-    tree_array = array.sort.uniq
-
-    start_index = 0
-    end_index = tree_array.length - 1
-    mid_index = (start_index + end_index) / 2
-
-    left_subarray = mid_index > 0 ? tree_array[0..(mid_index - 1)] : []
-    right_subarray = tree_array[(mid_index + 1)..end_index]
-
-    root_node = Node.new(tree_array[mid_index], build_tree(left_subarray), build_tree(right_subarray))
   end
 
   def insert(value)
@@ -199,9 +185,26 @@ class BinarySearchTree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
+
   private
 
   attr_writer :root
+
+  def build_tree(array)
+    return nil if array.length == 0
+    return Node.new(array[0]) if array.length == 1
+
+    tree_array = array.sort.uniq
+
+    start_index = 0
+    end_index = tree_array.length - 1
+    mid_index = (start_index + end_index) / 2
+
+    left_subarray = mid_index > 0 ? tree_array[0..(mid_index - 1)] : []
+    right_subarray = tree_array[(mid_index + 1)..end_index]
+
+    root_node = Node.new(tree_array[mid_index], build_tree(left_subarray), build_tree(right_subarray))
+  end
 end
 
 # Verify tree creation, balanced? check, and order traversals
